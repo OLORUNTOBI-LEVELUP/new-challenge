8de9843f4b9d77aa6fd669d3833f9b4c2844996e
OLORUNTOBI-LEVELUP <awoderuoloruntobi@gmail.com>
2019-01-23 17:08:11 +0100

    first commit

---
+++ answer1.js
@@ -0,0 +1,24 @@
+let levelUp = [
+    {
+       first_name: 'oloruntobi',
+       last_name: 'awoderu',
+       grade: 75,
+       age: 22,
+    },
+    {
+       
+        first_name: 'florence',
+        last_name: 'okunola',
+        grade: 89,
+        age: 28,
+    },
+    {
+        
+       first_name: 'tommiwa',
+       last_name: 'famosu',
+       grade: 73,
+       age: 25
+
+    }
+]
+console.log(levelUp)
